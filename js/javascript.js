980d38efe786612af2c4c58fd2c3bbaf23404f3b
Hoai-lan DINH VU <christiane.dinhvu@gmail.com>
2022-10-19 10:00:42 +0200

    initialisation de mon projet

---
+++ js/javascript.js
@@ -0,0 +1,28 @@
+// // Get the modal
+// var modal = document.getElementById("menu_info");
+
+// // Get the button that opens the modal
+// var menu = document.getElementById("menu_burger");
+
+// var menu_a = document.getElementById("menu_a");
+
+
+// // When the user clicks on the button, open the modal
+// menu.onclick = function() {
+//     modal.style.display = "block";
+//   }
+
+
+
+//   menu_a.onclick = function() {
+//     modal.style.display = "none";
+//   }
+  
+//   // When the user clicks anywhere outside of the modal, close it
+//   window.onclick = function(event) {
+//     if (event.target == modal) {
+//       modal.style.display = "none";
+//     }
+//   }
+
+
