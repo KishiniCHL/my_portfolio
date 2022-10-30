556a57ba97fa8b226851b07bc9d58e540c446c0f
Hoai-lan DINH VU <christiane.dinhvu@gmail.com>
2022-10-30 15:06:24 +0100

    responsive

--- js/javascript.js
+++ js/javascript.js
@@ -1,28 +1,33 @@
-// // Get the modal
-// var modal = document.getElementById("menu_info");
+// Get the modal
+var modal = document.getElementById("menu_info");
 
-// // Get the button that opens the modal
-// var menu = document.getElementById("menu_burger");
+// Get the button that opens the modal
+var menu = document.getElementById("menu_burger");
 
-// var menu_a = document.getElementById("menu_a");
-
-
-// // When the user clicks on the button, open the modal
-// menu.onclick = function() {
-//     modal.style.display = "block";
-//   }
+var menu_contact = document.getElementById("contact_link");
 
 
+// When the user clicks on the button, open the modal
+menu.onclick = function() {
+  if(modal.style.display === "block"){
+    modal.style.display = "none"
+  }
+  else{
+    modal.style.display = "block"
+  }
+}
 
-//   menu_a.onclick = function() {
-//     modal.style.display = "none";
-//   }
-  
-//   // When the user clicks anywhere outside of the modal, close it
-//   window.onclick = function(event) {
-//     if (event.target == modal) {
-//       modal.style.display = "none";
-//     }
-//   }
+
+
+menu_contact.onclick = function (){
+  modal.style.display = "none"
+  menu.checked = false;
+}
+
+  // window.onclick = function(event) {
+  //   if (event.target == modal) {
+  //     modal.style.display = "none";
+  //   }
+  // }
 
 
